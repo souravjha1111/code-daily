4e66d2832b5684af4b9b7851117343f84896ede6
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-01-22 12:57:56 +0530

    Time: 49 ms (63.09%), Space: 6.3 MB (75.17%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<bool> stones(n+1, false);
+        stones[1] = true;
+        
+        for(int i=2;i*i<=n;i++)
+        {
+            stones[i*i] = true;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            if(stones[i]==true)
+                continue;
+            
+            int idx = 1;
+            while(idx*idx<=n and i>=idx*idx)
+            {
+                if(stones[i-idx*idx]==false)
+                {
+                    stones[i] = true;
+                    break;
+                }
+                idx++;
+            }
+        }
+        return stones[n];
+    }
+};
