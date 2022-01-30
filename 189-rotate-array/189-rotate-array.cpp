793dff00dc5d1c346261f0f35634be1418d2cd0e
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-01-30 11:30:43 +0530

    Time: 67 ms (7.41%), Space: 25.1 MB (52.93%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+// [1,2,3,4,5,6,7]
+// [7,6,5,4,3,2,1]
+// [5,6,7,1,2,3,4]
