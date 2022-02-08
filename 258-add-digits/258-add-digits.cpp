ede8d8e2fce3f2d83068caaa6c57d679a1b8bc9f
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-02-08 09:08:18 +0530

    Time: 3 ms (53.62%), Space: 5.9 MB (25.37%) - LeetHub

---
+++ 258-add-digits/258-add-digits.cpp
@@ -0,0 +1,33 @@
+// class Solution {
+// public:
+//     int addDigits(int num) {
+//         if(num/10==0)
+//             return num;
+//         int temp =0;
+//         while(num/10 !=0){
+//             temp += num%10;
+//             num/=10;
+//         }
+//         if(num>0 && num%10==0)
+//             return addDigits(num/10);
+//         else{
+//         if(temp>0)
+//             temp+=num;
+//         return addDigits(temp);
+//     }}
+// };
+
+class Solution {
+public:
+    int addDigits(int num) {
+    int res =0;
+        while(0<num){
+            res+=num%10;
+            num/=10;
+            if(num==0 && res >9){
+                num = res;
+                res =0;}
+        }
+        return res;
+    }
+};
