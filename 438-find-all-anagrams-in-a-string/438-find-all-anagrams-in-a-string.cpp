05ba237b5629b2f04105adb750fc2cc6eec60ca9
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-02-11 20:53:52 +0530

    Time: 16 ms (71.61%), Space: 8.7 MB (73.04%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        if(p.length()>s.length())
-            return {};
-        int indx = 0;
-        int count=0;
-        vector<int> vec1(26,0);
+        vector<int> exp(26,0);
+        vector<int> curr(26,0);
+        for(auto el:p){
+            exp[el-'a']++;
+        }
         vector<int> res;
-        vector<int> vec2(26,0);
-        for(int i=0; i<p.length(); i++){
-            vec1[p[i]-'a']++;
-            count++;
-        }
-        for(int i=0; i<p.length(); i++){
-            vec2[s[i]-'a']++;
-        }
-        if(vec2==vec1)
-            res.push_back(0);
-        for(int i=1; i<=s.length()-p.length(); i++){
-            vec2[s[i-1] - 'a']--;
-            vec2[s[i+p.length()-1] - 'a']++;
-            if(vec2==vec1)
-                res.push_back(i);
-        }
+        for(int i=0; i<s.length(); i++){
+            curr[s[i]-'a']++;
+            if(i>=p.length())
+                curr[s[i-p.length()] - 'a']--;
+             if(curr==exp)
+                res.push_back(i-p.length()+1);
+            }
+        
         return res;
     }
 };
