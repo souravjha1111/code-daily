187c4be7968a8de426ce49a31d2edf5e2f993290
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-02-20 11:09:46 +0530

    Time: 44 ms (51.60%), Space: 20.1 MB (13.32%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,40 @@
+// class Solution {
+// public:
+// vector<vector<int>> merge(vector<vector<int>>& intervals) 
+//     {
+//         vector<vector<int>> res;
+//         sort(intervals.begin(),intervals.end());
+        
+//         res.push_back({intervals[0][0],intervals[0][1]});
+//         for(int i=1;i<intervals.size();i++)
+//         {
+//             if(res[res.size()-1][1]>=intervals[i][0])
+//             {   
+//                 int minm=min(res[res.size()-1][0],intervals[i][0]);
+//                 int maxm=max(res[res.size()-1][1],intervals[i][1]);
+//                 res[res.size()-1][0]=minm;
+//                 res[res.size()-1][1]=maxm;
+//             }
+//             else
+//                 res.push_back(intervals[i]);
+//         }
+//         return res;
+//     }
+// };    
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end());
+        res.push_back(intervals[0]);
+        for(auto el:intervals){
+            if(el[0]<=res[res.size()-1][1]){
+                res[res.size()-1][1] = max(el[1], res[res.size()-1][1]);
+            }
+            else{
+                res.push_back(el);
+            }
+        }
+        return res;
+    }
+};
