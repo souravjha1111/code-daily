15724fc7ecf3495356afa3995b1d8576718c0bbe
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-03-26 10:33:26 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (99.75%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,21 +1,58 @@
 class Solution {
 public:
-    vector<string>list={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> letterCombinations(string digits) {
-        vector<string>res;
-        if(digits.size()==0)
-            return res;
-        solve("",0,digits,res);
-        return res;   
-    }   
-    void solve(string currstr,int i,string digits,vector<string>&res){
-        if(i==digits.length()){
-            res.push_back(currstr);
-            return;
+        vector<string> buttons= {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> res={};
+        
+        int leng = digits.length();
+        if(leng==0)
+            return {};
+        else if(leng==1){
+            for(auto el:buttons[digits[0]-'0']){
+                string c = "";
+                c.push_back(el);
+                res.push_back(c);
+            }
         }
-        int dig=digits[i]-'0';
-        for(auto ch:list[dig]){
-            solve(currstr+ch,i+1,digits,res);
+        else if(leng==2){
+            for(auto el:buttons[digits[0]-'0'])
+                for(auto el1:buttons[digits[1]-'0']){
+                string c = "";
+                c.push_back(el);
+                c.push_back(el1);
+                res.push_back(c);
+                }
         }
+        
+        
+        else if(leng==3){
+            for(auto el:buttons[digits[0]-'0'])
+                for(auto el1:buttons[digits[1]-'0'])
+                    for(auto el2:buttons[digits[2]-'0']){
+                string c = "";
+                c.push_back(el);
+                c.push_back(el1);
+                c.push_back(el2);
+                res.push_back(c);
+                }
+        }
+        
+        
+        else if(leng==4){
+            for(auto el:buttons[digits[0]-'0'])
+                for(auto el1:buttons[digits[1]-'0'])
+                    for(auto el2:buttons[digits[2]-'0'])
+                        for(auto el3:buttons[digits[3]-'0']){
+                string c = "";
+                c.push_back(el);
+                c.push_back(el1);
+                c.push_back(el2);
+                c.push_back(el3);
+                res.push_back(c);
+                }
+        }
+        
+        
+        return res;
     }
 };
