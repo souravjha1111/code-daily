bb6c610c342d16a4fc0a007f43f4129099b7a165
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-04-18 01:11:58 +0530

    Time: 236 ms (66.67%), Space: 103.7 MB (100.00%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
-        priority_queue<pair<int, int>> q;
+        // priority_queue<pair<int, int>> q;
         unordered_map<int, int> mp;
         for(auto el:tasks){
             mp[el]++;
         }
-        for(auto el: mp){
-            q.push({el.second, el.first});
-        }
+        // for(auto el: mp){
+        //     q.push({el.second, el.first});
+        // }
+        // mp = {};
         int res =0;
-        while(q.size()!=0){
-            auto el = q.top();
-            if(el.first==1)
+        for(auto el : mp){
+            // auto el = q.top();
+            if(el.second==1)
                 return -1;
-            if(el.first %3==0){
-                res+= el.first/3;
+            if(el.second %3==0){
+                res+= el.second/3;
             }
-            else if(el.first%3==2){
-                res+=((el.first-2) /3)+1;
+            else if(el.second%3==2){
+                res+=((el.second-2) /3)+1;
             }
-            else if(el.first%3==1){
-                res+= (el.first/3 -1) +2;
+            else if(el.second%3==1){
+                res+= (el.second/3 -1) +2;
             }
-           else if(el.first %2==0){
-                res+= el.first/2;
+           else if(el.second %2==0){
+                res+= el.second/2;
             }
-            q.pop();
+            // q.pop();
         }
         return res;
     }
