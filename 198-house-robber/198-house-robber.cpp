a109637c20f33df27c8e8dcb247f47680c0e0aaa
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-04-21 01:02:03 +0530

    Time: 7 ms (10.56%), Space: 7.9 MB (25.52%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -8,11 +8,10 @@ public:
         else if(nums.size()==3)
             return max(nums[0] + nums[2], nums[1]);
         vector<int> dp(nums.size(), 0);
-        int n=nums.size();
-        dp[n-1] = nums[n-1];
-        dp[n-2] = nums[n-2];
-        dp[n-3] = nums[n-1] + nums[n-3];
-        for(int i = n-4; i>=0; i--){
+        dp[nums.size()-1] = nums[nums.size()-1];
+        dp[nums.size()-2] = nums[nums.size()-2];
+        dp[nums.size()-3] = nums[nums.size()-1] + nums[nums.size()-3];
+        for(int i = nums.size()-4; i>=0; i--){
             dp[i] = nums[i]+ max(dp[i+2], dp[i+3]);
         }
         return max(dp[0], dp[1]);
