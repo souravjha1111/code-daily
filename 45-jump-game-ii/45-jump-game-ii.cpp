ac33c2c7d70449511d5af5ba7232a6e84e2d0243
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-04-21 03:51:11 +0530

    Time: 20 ms (72.79%), Space: 16.6 MB (52.00%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,16 +1,29 @@
-class Solution {
+// class Solution {
+// public:
+// int jump(vector<int>& nums) {
+// 	vector<int> dp(size(nums), 10001);
+// 	return solve(nums, dp, 0);
+// }
+// int solve(vector<int>& nums, vector<int>& dp, int pos) {
+// 	if(pos >= size(nums) - 1)
+//         return 0; 
+// 	if(dp[pos] != 10001)
+// 	    return dp[pos];
+// 	for(int j = 1; j <= nums[pos]; j++)
+// 		dp[pos] = min(dp[pos], 1 + solve(nums, dp, pos + j));        
+// 	return dp[pos];
+// }
+// };
+class Solution{
 public:
+
 int jump(vector<int>& nums) {
-	vector<int> dp(size(nums), 10001);
-	return solve(nums, dp, 0);
-}
-int solve(vector<int>& nums, vector<int>& dp, int pos) {
-	if(pos >= size(nums) - 1)
-        return 0; 
-	if(dp[pos] != 10001)
-	return dp[pos];
-	for(int j = 1; j <= nums[pos]; j++)
-		dp[pos] = min(dp[pos], 1 + solve(nums, dp, pos + j));        
-	return dp[pos];
-}
-};
+        int res = 0, currpos = 0, maxjump = 0;
+        for(int i = 0 ; i < nums.size() - 1 ; i++) {
+            maxjump = max(maxjump, i + nums[i]);
+            if(i == currpos) {                
+                res++;                            
+                currpos = maxjump;
+            }                                            
+        }                                                
+        return res;}};
