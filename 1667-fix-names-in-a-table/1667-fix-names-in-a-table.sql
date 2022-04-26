94f4ac2e1994bc5e241e739aedf7823d0f51cb92
sourav jha <54894868+souravjha1111@users.noreply.github.com>
2022-04-26 09:12:43 +0530

    Time: 729 ms (29.03%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,7 +1,9 @@
-select
-    user_id,
-    concat(upper(substring(name,1, 1)), lower(substring(name, 2, length(name)-1))) as name
-from
-    users
-order by
-    user_id;
+select user_id, concat(upper(substring(name, 1,1)), lower(substring(name, 2, length(name)-1))) as name from Users order by user_id;
+
+# select
+#     user_id,
+#     concat(upper(substring(name,1, 1)), lower(substring(name, 2, length(name)-1))) as name
+# from
+#     users
+# order by
+#     user_id;
